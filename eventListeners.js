04d57b82d5c39b36de5ef2d1fa2f87c3b593303c
flobasel <buchmflo@students.zhaw.ch>
2020-12-08 13:23:11 +0100

    0812201323

--- eventListeners.js
+++ eventListeners.js
@@ -32,12 +32,28 @@ function handleModus (event) {
     let evData = ev.data; // the data from the argon event: "pressed" or "released"
     let evDeviceId = ev.coreid; // the device id
     let evTimestamp = Date.parse(ev.published_at); // the timestamp of the event
-
     let data = {
         message: evData, // just forward "started blinking" or "stopped blinking"
     }
 
     sendData("Modus", data, evDeviceId, evTimestamp );
+}
+
+function sendData(evName, evData, evDeviceId, evTimestamp ) {
+    
+    // map device id to device nr
+    let nr = exports.deviceIds.indexOf(evDeviceId)
+
+    // the message that we send to the client
+    let data = {
+        eventName: evName,
+        eventData: evData,
+        deviceNumber: nr,
+        timestamp: evTimestamp,
+    };
+
+    // send the data to all connected clients
+    exports.sse.send(data)
 }
 
 exports.deviceIds = [];
